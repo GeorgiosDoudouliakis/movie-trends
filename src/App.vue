d3e510057d170ebde2ee1177cdc9c5be2457a098
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-05 22:54:29 +0200

    Configure theme colors on tailwind config and apply some basic global styles

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,31 @@
 </template>
 
 <style lang="scss">
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+    scroll-behavior: smooth;
+  }
 
+  body {
+    min-height: 100vh;
+  }
+
+  h1, h2, h3, h4 {
+    @apply text-primary;
+  }
+
+  a, p, span {
+    @apply text-secondary;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    display: inline-block;
+    text-decoration: none;
+  }
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      'primary': '#1bbdb0',
+      'secondary': '#fcfcfc',
+      'lighter-black': '#0f1219'
+    },
     extend: {},
   },
   plugins: [],
 }
-
