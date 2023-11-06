5e320bc5c193baea5704c8920f2510b702f7dda6
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-06 22:08:29 +0200

    Change font size on footer

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="text-center text-sm py-3">
+  <footer class="text-center text-base py-3">
     <p>Copyright &copy; {{ currentDate }}. All Rights Reserved.</p>
   </footer>
 </template>
