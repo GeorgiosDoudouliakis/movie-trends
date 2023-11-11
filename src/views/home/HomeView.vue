2c6fc0ff0dff1d70836e65f55e6e61d5efdaab58
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-11 15:32:20 +0200

    Add a hero section on home page

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
+  <HomeHeroSection />
   <HomeTopRatedMovies />
   <HomeTopRatedTvSeries />
 </template>
 
 <script setup lang="ts">
+  import HomeHeroSection from "@/views/home/components/HomeHeroSection.vue";
   import HomeTopRatedMovies from "@/views/home/components/HomeTopRatedMovies.vue";
   import HomeTopRatedTvSeries from "@/views/home/components/HomeTopRatedTvSeries.vue";
 </script>
---
+++ src/views/home/components/HomeHeroSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative w-full hero-section-container">
+    <img class="absolute top-0 left-0 responsive-img" src="../../../assets/hero_img.jpg" alt="">
+    <div class="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center px-8">
+      <span class="text-2xl text-primary text-center mb-5">WELCOME TO MOVIETRENDS</span>
+      <p class="text-xl text-center">Explore movies, tv series and details for them from anywhere and anytime!</p>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .hero-section-container {
+    height: 280px;
+
+    > div {
+      content: "";
+      background: rgba(0, 0, 0, .8);
+
+      > p {
+        @media(min-width: 700px) {
+          width: 520px;
+        }
+      }
+    }
+  }
+</style>
