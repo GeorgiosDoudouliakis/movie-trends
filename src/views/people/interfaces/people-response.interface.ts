b5cebbc8c5c0d4badaa02c8816b62c34a7500e06
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-15 22:04:11 +0200

    Delete PersonKnowFor type and make known_for property on Person interface be of any type array

--- src/views/people/interfaces/people-response.interface.ts
+++ src/views/people/interfaces/people-response.interface.ts
@@ -1,12 +1,8 @@
-import { Movie, TvSerie } from "@/interfaces";
-
-export type PersonKnownFor = (Movie | TvSerie) & { type: "movie" | "tv" };
-
 export interface Person {
     adult: false;
     gender: number;
     id: number;
-    known_for: PersonKnownFor[];
+    known_for: any[];
     known_for_department: string;
     name: string;
     original_name: string;
