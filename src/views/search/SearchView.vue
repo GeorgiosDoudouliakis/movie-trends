6e6def08c41db9832a5e9ef098f4cb7e9e114cc4
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-19 14:59:46 +0200

    Add a search input on search page

--- src/views/search/SearchView.vue
+++ src/views/search/SearchView.vue
@@ -1,5 +1,8 @@
 <template>
-
+  <div class="flex items-center p-2 search-input">
+    <input class="p-2" type="text" placeholder="Search">
+    <button class="p-2 ml-4 btn-primary">Search</button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +10,22 @@
 </script>
 
 <style scoped lang="scss">
+  .search-input {
+    width: 90%;
+    max-width: 1280px;
+    margin: auto;
+    background: lighten(#0f1219, 10%);
 
+    input {
+      width: 100%;
+      color: var(--secondary-color);
+      background: lighten(#0f1219, 10%);
+      border: 1px solid var(--border-color);
+      outline: none;
+
+      &:focus {
+        border-color: var(--primary-color);
+      }
+    }
+  }
 </style>
