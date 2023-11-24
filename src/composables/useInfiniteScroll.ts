f141d1c3b1a2b6e221505df3fbde48665dd9d257
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-24 22:20:22 +0200

    Add a functionality on useInfiniteScroll to receive params

--- src/composables/useInfiniteScroll.ts
+++ src/composables/useInfiniteScroll.ts
@@ -2,15 +2,27 @@ import { onMounted, onUnmounted, readonly, Ref, ref } from "vue";
 import { BaseResponse } from "@/interfaces";
 
 export function useInfiniteScroll<ResponseType, ItemType extends { id: number; name: string; }>
-    (url: string, itemsMapper: (items: ItemType[]) => ItemType[]) {
+    (url: string, itemsMapper: (items: ItemType[]) => ItemType[], params?: { [key: string]: string }) {
     const items = ref<ItemType[]>([]) as Ref<ItemType[]>;
     const currentPage = ref<number>(1);
     const totalPages = ref<number>(1);
     const loading = ref<boolean>(true);
     const isOnLoadMore = ref<boolean>(false);
 
+    function requestUrl(page: number): string {
+        let request = `${url}&page=${page}`;
+
+        if(params) {    
+            Object.keys(params).forEach(param => {
+                request += `&${param}=${params[param]}`;
+            });
+        }
+
+        return request;
+    }
+
     function getItems(page: number) {
-        fetch(`${url}&page=${page}`)
+        fetch(requestUrl(page))
             .then(response => response.json())
             .then((response: BaseResponse<ResponseType>) => {
                 const mappedResults = itemsMapper(response.results as unknown as ItemType[]);
