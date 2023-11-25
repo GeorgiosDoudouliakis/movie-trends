6df7642e396d96def6d57596a4a5989db5a116da
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-25 16:13:00 +0200

    Fix useMapPosterPath composable

--- src/composables/useMapPosterPath.ts
+++ src/composables/useMapPosterPath.ts
@@ -1,6 +1,6 @@
 export function useMapPosterPath() {
     function mapPosterPath(width: number, posterPath: string): string {
-        return `https://image.tmdb.org/t/p/w${width}/${posterPath}`
+        return `https://image.tmdb.org/t/p/w${width}${posterPath}`
     }
 
     return { mapPosterPath };
