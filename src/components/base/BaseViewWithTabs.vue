b8a0e6dec4320ac9ddbd00d054de18c2bd9d3f39
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-28 00:17:13 +0200

    Add route name as h2 element on base view with tabs component

--- src/components/base/BaseViewWithTabs.vue
+++ src/components/base/BaseViewWithTabs.vue
@@ -1,22 +1,28 @@
 <template>
-  <div class="flex justify-between py-12 wrapper">
-    <div class="flex flex-col justify-center">
-      <router-link v-for="tab in tabs" :key="tab.id" :to="{ name: tab.name }" class="text-lg text-center py-4 text-secondary bg-card-bg">{{ tab.id }}</router-link>
-    </div>
-    <div class="ml-6">
-      <router-view></router-view>
+  <div class="py-12 wrapper">
+    <h2 class="text-2xl mb-10">{{ route.name }}</h2>
+    <div class="flex justify-between content">
+      <div class="flex flex-col justify-center">
+        <router-link v-for="tab in tabs" :key="tab.id" :to="{ name: tab.name }" class="text-center py-4 text-secondary bg-card-bg">{{ tab.id }}</router-link>
+      </div>
+      <div class="ml-6">
+        <router-view></router-view>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { useRoute } from "vue-router";
   import { Tab } from '@/interfaces';
+
+  const route = useRoute();
 
   defineProps<{ tabs: Tab[] }>()
 </script>
 
 <style scoped lang="scss">
-  .wrapper {
+  .content {
     > div {
       &:first-of-type {
         width: 180px;
