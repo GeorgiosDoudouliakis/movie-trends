de8183fb5a875a999f792bf527a0c4fc6df3ff01
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-28 00:05:06 +0200

    Add width on child divs of base view with tabs component

--- src/components/base/BaseViewWithTabs.vue
+++ src/components/base/BaseViewWithTabs.vue
@@ -1,27 +1,39 @@
 <template>
-  <div class="flex py-12 wrapper">
+  <div class="flex justify-between py-12 wrapper">
     <div class="flex flex-col justify-center">
-      <router-link v-for="tab in tabs" :key="tab.id" :to="{ name: tab.name }" class="text-lg text-center p-4 text-secondary bg-card-bg">{{ tab.id }}</router-link>
+      <router-link v-for="tab in tabs" :key="tab.id" :to="{ name: tab.name }" class="text-lg text-center py-4 text-secondary bg-card-bg">{{ tab.id }}</router-link>
     </div>
-    <div class="ml-4">
+    <div class="ml-6">
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-    import { Tab } from '@/interfaces';
+  import { Tab } from '@/interfaces';
 
-    defineProps<{ tabs: Tab[] }>()
+  defineProps<{ tabs: Tab[] }>()
 </script>
 
 <style scoped lang="scss">
-    a:not(:last-of-type) {
-        margin-bottom: 10px;
-    }
+  .wrapper {
+    > div {
+      &:first-of-type {
+        width: 180px;
 
-    .router-link-exact-active {
-      @apply text-primary;
-      border-left: 4px solid var(--primary-color);
+        a:not(:last-of-type) {
+          margin-bottom: 10px;
+        }
+
+        .router-link-exact-active {
+          @apply text-primary;
+          border-left: 4px solid var(--primary-color);
+        }
+      }
+
+      &:last-of-type {
+        width: calc(100vh - 180px);
+      }
     }
+  }
 </style>
