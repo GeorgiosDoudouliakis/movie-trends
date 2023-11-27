2123cfc9b1d0d5f473d11dad29b5640ced724121
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-27 22:50:30 +0200

    Add components for movies and tv series children views and routing for them

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,17 +16,87 @@ const routes: Array<RouteRecordRaw> = [
     path: '/movies',
     name: 'Movies',
     component: () => import("../views/movies/MoviesView.vue"),
-    meta: {
-      title: 'Movies'
-    }
+    children: [
+      {
+        path: "/now-playing",
+        name: 'Now playing movies',
+        component: () => import("../views/movies/NowPlayingMoviesView.vue"),
+        meta: {
+          title: 'Now playing movies'
+        },
+      },
+      {
+        path: "/popular",
+        name: 'Popular movies',
+        component: () => import("../views/movies/PopularMoviesView.vue"),
+        meta: {
+          title: 'Popular movies'
+        },
+      },
+      {
+        path: "/top-rated",
+        name: 'Top rated movies',
+        component: () => import("../views/movies/TopRatedMoviesView.vue"),
+        meta: {
+          title: 'Top rated movies'
+        },
+      },
+      {
+        path: "/upcoming",
+        name: 'Upcoming movies',
+        component: () => import("../views/movies/UpcomingMoviesView.vue"),
+        meta: {
+          title: 'Upcoming movies'
+        },
+      },
+      {
+        path: "",
+        redirect: { name: "Now playing movies" }
+      }
+    ]
   },
   {
     path: '/tv-series',
     name: 'TvSeries',
     component: () => import("../views/tv-series/TvSeriesView.vue"),
-    meta: {
-      title: 'Tv Series'
-    }
+    children: [
+      {
+        path: "/airing-today",
+        name: 'Airing today tv series',
+        component: () => import("../views/tv-series/AiringTodayTvSeriesView.vue"),
+        meta: {
+          title: 'Airing today tv series'
+        },
+      },
+      {
+        path: "/on-the-air",
+        name: 'On the air tv series',
+        component: () => import("../views/tv-series/OnTheAirTvSeriesView.vue"),
+        meta: {
+          title: 'On the air tv series'
+        },
+      },
+      {
+        path: "/popular",
+        name: 'Popular tv series',
+        component: () => import("../views/tv-series/PopularTvSeriesView.vue"),
+        meta: {
+          title: 'Popular tv series'
+        },
+      },
+      {
+        path: "/top-rated",
+        name: 'Top rated tv series',
+        component: () => import("../views/tv-series/TopRatedTvSeriesView.vue"),
+        meta: {
+          title: 'Top rated tv series'
+        },
+      },
+      {
+        path: "",
+        redirect: { name: "Airing today tv series" }
+      }
+    ]
   },
   {
     path: "/people",
