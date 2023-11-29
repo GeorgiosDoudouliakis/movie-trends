509f899131326343dc355b87e7d2cb6732132579
Georgios Doudouliakis <dudugeorge89@gmail.com>
2023-11-29 21:46:48 +0200

    Make a base movies list component and use it on all movies subpages

--- src/views/movies/TopRatedMoviesView.vue
+++ src/views/movies/TopRatedMoviesView.vue
@@ -1,11 +1,14 @@
 <template>
-
+  <BaseMoviesList :items="items" :loading="loading" :is-on-load-more="isOnLoadMore"/>
 </template>
 
 <script setup lang="ts">
+  import { useInfiniteScroll } from "@/composables/useInfiniteScroll";
+  import { BaseResponse, Movie } from "@/interfaces";
+  import { onMounted } from "vue";
+  import BaseMoviesList from "@/views/movies/components/BaseMoviesList.vue";
 
+  const { items, loading, isOnLoadMore, getItems } = useInfiniteScroll<BaseResponse<Movie>, Movie>('https://api.themoviedb.org/3/movie/top_rated?api_key=803a77b2748b6f5d6363b4fa92bfd870&language=en-US');
+
+  onMounted(() => getItems())
 </script>
-
-<style scoped lang="scss">
-
-</style>
---
+++ src/views/movies/components/BaseMoviesList.vue
@@ -0,0 +1,30 @@
+<template>
+  <template v-if="!loading">
+    <div class="flex flex-wrap justify-center gap-6">
+      <template v-for="item in items" :key="item.id">
+        <BaseCard :name="item.title" :image="{ src: item.poster_path, alt: item.title, width: 185 }" direction="vertical">
+          <span class="text-center">{{ mapReleaseDate(item.release_date) }}</span>
+        </BaseCard>
+      </template>
+    </div>
+  </template>
+
+  <div v-if="loading || isOnLoadMore" class="flex justify-center">
+    <BaseLoader/>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Movie } from "@/interfaces";
+  import BaseLoader from "@/components/base/BaseLoader.vue";
+  import BaseCard from "@/components/base/BaseCard.vue";
+  import { useMapReleaseDate } from "@/composables/useMapReleaseDate";
+
+  defineProps<{
+    items: Movie[],
+    loading: boolean,
+    isOnLoadMore: boolean
+  }>()
+
+  const { mapReleaseDate } = useMapReleaseDate();
+</script>
